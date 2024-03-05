9dd238cdba7517a668e1e21b6050ad45741905ef
einge23 <einge823@gmail.com>
2024-03-05 10:18:38 -0500

    add separate file for smartcandlestick

---
+++ WindowsForms COP4365 001/SmartCandlestick.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms_COP4365_001
+{
+    public class SmartCandlestick : Candlestick
+    {
+        public decimal range { get; set; }
+        public decimal topPrice { get; set; }
+        public decimal bottomPrice { get; set; }
+        public decimal bodyRange { get; set; }
+        public decimal upperTail { get; set; }
+        public decimal lowerTail { get; set; }
+
+        public bool isBullish { get; set; }
+        public bool isBearish { get; set; }
+        public bool isNeutral { get; set; }
+        public bool isDoji { get; set; }
+        public bool isHammer { get; set; }
+        public bool isMarubozu { get; set; }
+
+        SmartCandlestick(string RowOfData) : base(RowOfData)
+        {
+            ComputeExtraProperties();
+            ComputePatternProperties();
+        }
+
+
+        public void ComputeExtraProperties()
+        {
+            range = high - low;
+            topPrice = Math.Max(open, close);
+            bottomPrice = Math.Min(open, close);
+            bodyRange = topPrice - bottomPrice;
+            upperTail = high - topPrice;
+            lowerTail = bottomPrice - low;
+        }
+
+
+
+        public void ComputePatternProperties()
+        {
+            isBearish = (open > close);
+            isBullish = (open < close);
+            isNeutral = (close == open);
+            isMarubozu = (bodyRange >= (decimal)0.96 * range);
+            isHammer = ((lowerTail >= (decimal)0.7 * range) || (upperTail >= (decimal)0.7 * range));
+            isDoji = (bodyRange <= (decimal)0.1 * range);
+
+        }
+
+    }
+}
+
