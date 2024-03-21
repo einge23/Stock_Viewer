eeaf43b41098659a2b677c3b8fcb0ee93c13c282
einge23 <einge823@gmail.com>
2024-03-21 10:40:53 -0400

    make part3 and add recognize bullish class

---
+++ WindowsForms COP4365 001/Recognizer_Bullish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms_COP4365_001
+{
+    internal class Recognizer_Bullish
+    {
+        bool Recognize(SmartCandlestick scs)
+        {
+            return scs.properties["Bullish"];
+        }
+    }
+}
