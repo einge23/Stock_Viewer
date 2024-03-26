8972ce360a944b0167754aa1c65e8bf92864fec9
einge23 <einge823@gmail.com>
2024-03-26 10:48:10 -0400

    add all recognizers

---
+++ WindowsForms COP4365 001/Recognizer_Hanami.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms_COP4365_001
+{
+    internal class Recognizer_Hanami
+    {
+    }
+}
